5053287bbebac63d7260b3250cfcef9c7225d1f6
khashayar <aripastudio44@gmail.com>
2025-06-16 08:39:51 -0700

    Start

---
+++ NumberGuessingGame/NumberGuessing.c
@@ -0,0 +1,5 @@
+//
+// Created by VENUS on 6/16/2025.
+//
+
+#include "NumberGuessing.h"
---
+++ NumberGuessingGame/NumberGuessing.h
@@ -0,0 +1,8 @@
+//
+// Created by VENUS on 6/16/2025.
+//
+
+#ifndef NUMBERGUESSING_H
+#define NUMBERGUESSING_H
+
+#endif //NUMBERGUESSING_H
